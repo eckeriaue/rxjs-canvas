d98383d329690b9c59b67f59e0482855db85f728
Elberd Chakhkiev <114188050+eckeriaue@users.noreply.github.com>
2022-11-12 21:11:47 +0300

    -

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,19 @@
+import { fromEvent } from "rxjs"
+import { map, debounceTime, distinctUntilChanged, switchMap } from 'rxjs/operators'
+import { ajax } from 'rxjs/ajax'
+import type { Observable } from "rxjs/internal/Observable"
+
 const api: string = 'http://github.com/search/users?q='
+
+const search: HTMLElement = document.getElementById('search')
+const stream$: Observable<Event> = fromEvent(search, 'input')
+  .pipe(
+    map( (e: any) => e.target.value ),
+    debounceTime(1000),
+    distinctUntilChanged(),
+    switchMap(v => ajax.getJSON(api + v))
+  )
+
+stream$.subscribe(value => {
+  console.log(value)
+})
