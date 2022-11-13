e375c7728d7f9728023dfd1a6dd1ee9b1ea5fff4
Elberd Chakhkiev <114188050+eckeriaue@users.noreply.github.com>
2022-11-13 19:21:05 +0300

    prettier

--- src/ts/clear.ts
+++ src/ts/clear.ts
@@ -1,8 +1,12 @@
 import { fromEvent } from 'rxjs'
 import { ctx, canvas } from './canvas'
 
-const clearBtn: HTMLElement = document.getElementById('clear')
-/* const clear$ = */
-fromEvent(clearBtn, 'click').subscribe(() => {
+namespace Stream {
+  const clearBtn: HTMLElement = document.getElementById('clear')
+  export const clear$ = fromEvent(clearBtn, 'click')
+}
+
+Stream.clear$.subscribe(() => {
   ctx.clearRect(0, 0, canvas.width, canvas.height)
 })
+
